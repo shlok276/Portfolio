994910c439eef53ba956f76ee3fdf2a2745c40ff
shlok276 <patelshlokhasmukhbhai21502@gmail.com>
2025-11-22 03:35:27 -0700

    project optimized

--- main.js
+++ main.js
@@ -9,3 +9,33 @@ function toggleNavbar() {
     const navbar = document.querySelector('.navbar');
     navbar.classList.toggle('active');
 }
+<div class="skill">
+                    <h3>Frameworks & Tools</h3>
+                    <div class="skill-name">
+                        <a>Flask</a>
+                        <a>Bootstrap</a>
+                        <a>Git</a>
+                        <a>GitHub</a>
+                        <a>GitLab</a>
+                        <a>VSCode</a>
+                        <a>WordPress</a>
+                    </div>
+                </div>
+                
+                <div class="skill">
+                    <h3>Productivity & Office</h3>
+                    <div class="skill-name">
+                        <a>Word</a>
+                        <a>PowerPoint</a>
+                        <a>Excel</a>
+                        <a>Outlook</a>
+                    </div>
+                </div>
+                
+                <div class="skill">
+                    <h3>Dev Methods & Design</h3>
+                    <div class="skill-name">
+                        <a>SDLC</a>
+                        <a>UML</a>
+                    </div>
+                </div>
